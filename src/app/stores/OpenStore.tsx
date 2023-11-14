24174090ea3122f726d30a88ad0cf62bee6ca672
Alberto Araújo <alberto.araujoo@hotmail.com>
2023-11-14 20:00:17 -0300

    fix

--- src/app/stores/OpenStore.tsx
+++ src/app/stores/OpenStore.tsx
@@ -1,7 +1,13 @@
 "use client";
-import { create } from "zustand";
+import create from "zustand";
 
-export const useOpenStore = create((set) => ({
+type OpenStore = {
+  open: boolean;
+  setOpenTrue: () => void;
+  setOpenFalse: () => void;
+};
+
+export const useOpenStore = create<OpenStore>((set) => ({
   open: false,
   setOpenTrue: () => set({ open: true }),
   setOpenFalse: () => set({ open: false }),
